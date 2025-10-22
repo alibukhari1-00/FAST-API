37a377afc1afef5f0cb8ff4e5d18241623eeffb2
Ali <i.alibukhari1@gmail.com>
2025-10-23 03:06:34 +0500

    Update Changes PostMethod

--- post.py
+++ post.py
@@ -1,58 +1,83 @@
-from fastapi import FastAPI
-from pydantic import BaseModel, Field, field_validator, EmailStr, confloat
-from typing import Annotated, Optional
+from fastapi import FastAPI,Path,HTTPException
+from pydantic import BaseModel,Field,field_validator,EmailStr,AnyUrl,confloat
 from fastapi.responses import JSONResponse
+from typing import Annotated,Optional,List,Dict
 import json
 
-app1 = FastAPI()
 
-class Address(BaseModel):
-    postal_code: int = Field(ge=10000, le=99999)
-    street: str = Field(max_length=30, title="Street")
-    city: str = Field(max_length=12)
-    province: str = Field(max_length=15)
-    country_of_stay: str = Field(max_length=15)
-
+app1=FastAPI()
+class address(BaseModel):
+    postal_code:int =Field(max_length=5)
+    street:str=Field(max_length=30,title="Street")
+    city:str=Field(max_length=12)
+    province:str=Field(max_length=15)
+    countryOFstay:str=Field(max_length=15)
     @field_validator("province")
     @classmethod
-    def validate_province(cls, value):
-        provinces = ["Punjab","Sindh","kpk","Balochistan"]
-        if value not in provinces:
-            raise ValueError("Given Province is not valid")
-        return value
-
+    def validate(cls,value):
+        province=["Punjab","Sindh","kpk","Balochistan"]
+        if value not in province:
+          raise ValueError("Given Province is not valid")
+        
 class Student(BaseModel):
-    name: Annotated[str, Field(max_length=15, title="Name Of Student")]
-    Cnic: Annotated[str, Field(max_length=15, title="CNIC of Student", example="33100-1234567-1")]
-    address: Address
-    Guardian_name: Annotated[str, Field(max_length=15)]
-    Department_name: Annotated[str, Field(max_length=20, example="AI")]
-    Cgpa: Optional[Annotated[confloat(ge=0, le=4), Field(title="CGPA", example=3.75)]] = None
-    email: EmailStr
-    age: int = Field(ge=15, le=23)
-    Matric_marks: confloat(ge=0, le=1100) = Field(title="Matriculation Marks")
-    inter_marks: confloat(ge=0, le=1100) = Field(title="Intermediate Marks")
+    name:Annotated[str,Field(max_length=15,title="Name Of Student",description="Please Enter Your Name According to Nadra")]
+    Cnic:Annotated[str,Field(max_length=15,title="CNIC of Student",description="Please Enter your Correct Cnic Number",example="33xxx-xxxxxxx-xx")]
+    address:address
+    Guardian_name:Annotated[str,Field(max_length=15,title="Guardian Name",description="Please Enter the Name of Your Guardian")]
+    Department_name:Annotated[str,Field(max_length=20,title="Department Name",description="Please Enter Name of Your Department",example=["AI","CS","SE","EE"])]
+    Cgpa:Optional[Annotated[float,Field(title="CGPA",description="Please enter your CGPA (0.0 - 4.0)",example=3.75)]]=None
+    email:EmailStr
+    age:int=Field(ge=15,le=23)
+    Matric_marks:confloat=Field(ngte=1100,title="Matriculation Marks",description="Please Enter your Matric Marks")
+    inter_marks:confloat=Field(ngte=1100,title="Intermediate Marks",description="Please Enter your Intermediate Marks")
 
     @field_validator("Department_name")
     @classmethod
-    def validate_dept(cls, value):
-        dept_name = ["AI","CS","SE","EE"]
+    def validate_dept(cls,value):
+        dept_name=["AI","CS","SE","EE"]
         if value not in dept_name:
             raise ValueError("Department Name is not Valid")
         return value
-
+    
     @field_validator("Cnic")
     @classmethod
-    def validate_cnic(cls, value):
-        if len(value) != 15:
-            raise ValueError("Length of CNIC is not valid")
-        if value[5] != "-" or value[13] != "-":
-            raise ValueError("Dashes are missing or not at correct places")
+    def validate_cnic(cls,value):
+        if len(value)!=15:
+            raise ValueError("Lenght of Cnic is not valid !!!")
+        if value[5]!="-":
+            raise ValueError("Dashes are missing or not at correct Place")
+        if value[13]!="-":
+            raise ValueError("Dashes are missing or not at correct Place")
         return value
 
+
+data={1:"ali", 2:"waqas", 3:"Mian", 4:"Suffian", 5:"Mohsin"}
+
+@app1.get("/")
+def read_root():
+    return {"message": "Hello FastAPI!"}
+
+@app1.get("/about")
+def about():
+    return {"message":"This is the about section of my api"}
+
+@app1.get("/view")
+def load_data():
+    return data
+
+
+@app1.get("/info/{id}")
+def get_info(id: int=Path(...,description="Id is of the format 1 ,2 ,3 ",title="Patient Data ")):
+    if id in data:
+        return {"Info found Successfully ":data[id]}
+    else:
+        raise HTTPException(status_code=404,detail="Information not found")
+
+
+
 @app1.post("/create")
-def create_student(student1: Student):
-    data = student1.model_dump()
-    with open('student_data.json', "w") as f:
-        json.dump(data, f)
-    return JSONResponse(status_code=201, content=["Student Created Successfully"])
+def create_student(student1:Student):
+    data=student1.model_dump()
+    with open('student_data',"w") as f:
+        json.dump(data,f)
+    return JSONResponse(status_code=201,content=['Student Created Successfully'])
---
+++ postmethod.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI
+from pydantic import BaseModel,Field,field_validator,EmailStr,AnyUrl,confloat
+from fastapi.responses import JSONResponse
+from typing import Annotated,Optional,List,Dict
+import json
+
+
+app1=FastAPI()
+class address(BaseModel):
+    postal_code:int =Field(max_length=5)
+    street:str=Field(max_length=30,title="Street")
+    city:str=Field(max_length=12)
+    province:str=Field(max_length=15)
+    countryOFstay:str=Field(max_length=15)
+    @field_validator
+    @classmethod
+    def validate(cls,value):
+        province=["Punjab","Sindh","kpk","Balochistan"]
+        if value not in province:
+          raise ValueError("Given Province is not valid")
+        
+class Student(BaseModel):
+    name:Annotated[str,Field(max_length=15,title="Name Of Student",description="Please Enter Your Name According to Nadra")]
+    Cnic:Annotated[str,Field(max_length=15,title="CNIC of Student",description="Please Enter your Correct Cnic Number",example="33xxx-xxxxxxx-xx")]
+    address:address
+    Guardian_name:Annotated[str,Field(max_length=15,title="Guardian Name",description="Please Enter the Name of Your Guardian")]
+    Department_name:Annotated[str,Field(max_length=20,title="Department Name",description="Please Enter Name of Your Department",example=["AI","CS","SE","EE"])]
+    Cgpa:Optional[Annotated[float,Field(title="CGPA",description="Please enter your CGPA (0.0 - 4.0)",example=3.75)]]=None
+    email:EmailStr
+    age:int=Field(ge=15,le=23)
+    Matric_marks:confloat=Field(ngte=1100,title="Matriculation Marks",description="Please Enter your Matric Marks")
+    inter_marks:confloat=Field(ngte=1100,title="Intermediate Marks",description="Please Enter your Intermediate Marks")
+
+    @field_validator("Department_name")
+    @classmethod
+    def validate_dept(cls,value):
+        dept_name=["AI","CS","SE","EE"]
+        if value not in dept_name:
+            raise ValueError("Department Name is not Valid")
+        return value
+    
+    @field_validator("Cnic")
+    @classmethod
+    def validate_cnic(cls,value):
+        if len(value)!=15:
+            raise ValueError("Lenght of Cnic is not valid !!!")
+        if value[5]!="-":
+            raise ValueError("Dashes are missing or not at correct Place")
+        if value[13]!="-":
+            raise ValueError("Dashes are missing or not at correct Place")
+        return value
+
+
+@app1.post("/create")
+def create_student(student1:Student):
+    data=student1.model_dump()
+    with open('student_data',"w") as f:
+        json.dump(data,f)
+    return JSONResponse(status_code=201,content=['Student Created Successfully'])
